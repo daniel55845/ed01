e693689927caba70e0d8121f1233f2bb79e55bbe
daniel55845 <danieltomazdalcortivo@gmail.com>
2025-04-17 15:35:46 -0300

    Update Jogador.java

--- Jogador.java
+++ Jogador.java
@@ -1,16 +1,29 @@
 package ed01;
 
+/**
+ * Armazena e gerencia os dados de um jogador no jogo de adivinhação.
+ * @author Daniel Tomaz Dal Cortivo
+ */
+
 public class Jogador {
     private String nome;
     private int tentativas;
     private int pontuacao;
 
+    /**
+     * Cria um novo jogador com um nome.
+     * @param nome Nome do jogador 
+     */
     public Jogador(String nome) {
         this.nome = nome;
         this.tentativas = 0;
         this.pontuacao = 0;
     }
 
+    /**
+     *  Métodos de get e set dos atributos @param nome, @param tentativas, @param pontuacao
+     */
+    
     public String getNome() {
         return nome;
     }
@@ -35,10 +48,17 @@ public class Jogador {
         this.pontuacao = pontuacao;
     }
 
+    /**
+     * Incrementa em 1 o contador de tentativas.
+     */
     public void incrementarTentativas() {
         this.tentativas++;
     }
 
+    /**
+     * Adiciona valor a pontuação atual .
+     * @param valor acresenta valor a pontucao
+     */
     public void atualizarPontuacao(int valor) {
         this.pontuacao += valor;
     }
